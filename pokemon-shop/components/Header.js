c9edc1a03bc481b0c29334703b7e45030cb2abda
alex damsted <alexdamsted@gmail.com>
2021-07-13 16:33:29 +1200

    Create a Header component

---
+++ pokemon-shop/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>yp</h1>
+    </header>
+  );
+}
+
+export default Header;
--- pokemon-shop/pages/index.js
+++ pokemon-shop/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
+import Header from "../components/Header";
 
 export default function Home() {
-  return <p>hi</p>;
+  return <Header />;
 }
