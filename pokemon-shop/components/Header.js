6f404800d688049c80bf21e2bbc1d4ee7fa8e555
alex damsted <alexdamsted@gmail.com>
2021-07-14 15:39:52 +1200

    Add a gap between header items on min:sm

--- pokemon-shop/components/Header.js
+++ pokemon-shop/components/Header.js
@@ -9,7 +9,7 @@ import Search from "./Search";
 function Header() {
   return (
     <header>
-      <div className="w-1/2 m-auto sm:w-full sm:flex sm:justify-center">
+      <div className="w-full m-auto sm:w-full sm:flex sm:justify-center sm:gap-5">
         <HeaderItem title="Home" Icon={HomeIcon} />
         <HeaderItem title="Cart" Icon={ShoppingCartIcon} />
         <Search Icon={SearchIcon} placeholder="Pokemon Search..." />
