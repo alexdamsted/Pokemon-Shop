073c38e1ace300841d202b8a2b95c32f97d8e966
alex damsted <alexdamsted@gmail.com>
2021-07-14 14:14:46 +1200

    Flex and position styles for HeaderItems

--- pokemon-shop/components/HeaderItem.js
+++ pokemon-shop/components/HeaderItem.js
@@ -1,9 +1,14 @@
 function HeaderItem({ title, Icon }) {
   return (
-    <div>
-      <Icon />
-      {title}
-    </div>
+    <>
+      <div
+        className="flex flex-col items-center cursor-pointer group
+    w-12 sm:w-20 hover:text-white"
+      >
+        <Icon className="h-8 mb-1" />
+        <p className="opacity-100 group-hover:opacity-100">{title}</p>
+      </div>
+    </>
   );
 }
 
