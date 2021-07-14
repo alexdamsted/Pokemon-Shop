09039671685d32dfa414ee1239439afec1cb3711
alex damsted <alexdamsted@gmail.com>
2021-07-14 15:40:12 +1200

    Display hidden on header item text

--- pokemon-shop/components/HeaderItem.js
+++ pokemon-shop/components/HeaderItem.js
@@ -5,8 +5,8 @@ function HeaderItem({ title, Icon }) {
         className="cursor-pointer group flex flex-col items-center
       hover:text-white"
       >
-        <Icon className="h-8 mb-1" />
-        <p className="opacity-100 group-hover:opacity-100">{title}</p>
+        <Icon className="h-8" />
+        <p className="hidden group-hover:block">{title}</p>
       </div>
     </>
   );
