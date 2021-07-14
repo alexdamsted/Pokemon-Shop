ea1b8ab7185c92ff229f0f2a179ba8cd1be6584b
alex damsted <alexdamsted@gmail.com>
2021-07-14 16:03:26 +1200

    Input cursor and user input text wasn't centering

--- pokemon-shop/components/Search.js
+++ pokemon-shop/components/Search.js
@@ -4,7 +4,7 @@ function Search({ Icon, placeholder }) {
       {" "}
       <input
         type="text"
-        class="w-full text-center py-2 pl-6 rounded z-0 focus:shadow focus:outline-none"
+        class="w-full text-center text-gray-500 py-2 rounded z-0 focus:shadow focus:outline-none"
         placeholder={placeholder}
       />
       <Icon className="absolute ml-1 h-6 w-6 text-gray-400 z-20 hover:text-gray-500"></Icon>
