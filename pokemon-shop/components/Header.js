bacb83a65d7a1af208ec40beaed6a4f98d21b3aa
alex damsted <alexdamsted@gmail.com>
2021-07-14 15:31:39 +1200

    Mobile position of header items block and centered

--- pokemon-shop/components/Header.js
+++ pokemon-shop/components/Header.js
@@ -9,7 +9,7 @@ import Search from "./Search";
 function Header() {
   return (
     <header>
-      <div className="">
+      <div className="w-1/2 m-auto sm:w-full sm:flex sm:justify-center">
         <HeaderItem title="Home" Icon={HomeIcon} />
         <HeaderItem title="Cart" Icon={ShoppingCartIcon} />
         <Search Icon={SearchIcon} placeholder="Pokemon Search..." />
--- pokemon-shop/components/HeaderItem.js
+++ pokemon-shop/components/HeaderItem.js
@@ -2,8 +2,8 @@ function HeaderItem({ title, Icon }) {
   return (
     <>
       <div
-        className="flex flex-col items-center cursor-pointer group
-    w-12 sm:w-20 hover:text-white"
+        className="cursor-pointer group flex flex-col items-center
+      hover:text-white"
       >
         <Icon className="h-8 mb-1" />
         <p className="opacity-100 group-hover:opacity-100">{title}</p>
