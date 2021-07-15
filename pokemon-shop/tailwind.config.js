b0e7588a5bb102c33e406de17c8d6734c169b5da
alex damsted <alexdamsted@gmail.com>
2021-07-15 15:44:52 +1200

    Add a font-family

--- pokemon-shop/tailwind.config.js
+++ pokemon-shop/tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: { sans: ["Helvetica", "Arial", "sans-serif"] },
     extend: {},
   },
   variants: {
