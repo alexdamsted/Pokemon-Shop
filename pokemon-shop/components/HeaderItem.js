2c5dd7a0de2554509ef9b860a9f6f6e4849378c4
alex damsted <alexdamsted@gmail.com>
2021-07-20 12:31:09 +1200

    Turn header item text boldness down

--- pokemon-shop/components/HeaderItem.js
+++ pokemon-shop/components/HeaderItem.js
@@ -3,7 +3,7 @@ function HeaderItem({ title, Icon }) {
     <>
       <div className="flex items-center my-4 cursor-pointer group hover:text-white">
         <Icon className="h-8" />
-        <p className="ml-1 text-xl font-semibold sm:text-3xl">{title}</p>
+        <p className="ml-1 text-xl font-medium sm:text-3xl">{title}</p>
       </div>
     </>
   );
