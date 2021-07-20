da791d63e0b4d133951c0f84d7199e7c809c5edc
alex damsted <alexdamsted@gmail.com>
2021-07-20 12:32:10 +1200

    Add a hover:underline to each header item

--- pokemon-shop/components/HeaderItem.js
+++ pokemon-shop/components/HeaderItem.js
@@ -1,7 +1,7 @@
 function HeaderItem({ title, Icon }) {
   return (
     <>
-      <div className="flex items-center my-4 cursor-pointer group hover:text-white">
+      <div className="flex items-center my-4 cursor-pointer group hover:text-white hover:underline">
         <Icon className="h-8" />
         <p className="ml-1 text-xl font-medium sm:text-3xl">{title}</p>
       </div>
