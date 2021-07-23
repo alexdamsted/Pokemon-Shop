d5c48638ea1eb4f777c13936eb6180ecedb87c6e
alex damsted <alexdamsted@gmail.com>
2021-07-23 16:33:16 +1200

    Data from PokeAPI now displaying on screen

--- pokemon-shop/components/Content.js
+++ pokemon-shop/components/Content.js
@@ -1,6 +1,6 @@
 const Content = ({ pokemon }) => {
-  console.log(pokemon.results[0]);
-  return <div></div>;
+  console.log(pokemon.results[0].name);
+  return <div>{pokemon.results[0].name}</div>;
 };
 
 export default Content;
