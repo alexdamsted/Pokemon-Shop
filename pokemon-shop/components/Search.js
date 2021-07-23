0ccba4f12f84e00ef94097e4da2f7c4543aaeb27
alex damsted <alexdamsted@gmail.com>
2021-07-23 16:30:08 +1200

    ClassName syntax error fix

--- pokemon-shop/components/Search.js
+++ pokemon-shop/components/Search.js
@@ -1,10 +1,10 @@
 function Search({ Icon, placeholder }) {
   return (
-    <div class="flex items-center mt-10 my-4 w-full">
+    <div className="flex items-center mt-10 my-4 w-full">
       {" "}
       <input
         type="text"
-        class="w-full text-center text-2xl text-gray-500 py-3 rounded z-0 focus:shadow focus:outline-none md:text-3xl md:py-5"
+        className="w-full text-center text-2xl text-gray-500 py-3 rounded z-0 focus:shadow focus:outline-none md:text-3xl md:py-5"
         placeholder={placeholder}
       />
       <Icon className="absolute ml-1 h-6 w-6 text-gray-400 z-20 hover:text-gray-500 md:ml-2 md:h-10 md:w-10"></Icon>
