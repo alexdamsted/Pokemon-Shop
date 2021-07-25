5a2b800c2d1989edfd724661f8fe0dfd3ce2caa4
alex damsted <alexdamsted@gmail.com>
2021-07-25 16:20:44 +1200

    Capitalize first letter of each Pokemons' name

--- pokemon-shop/components/Content.js
+++ pokemon-shop/components/Content.js
@@ -2,15 +2,16 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
 const Content = ({ pokemon }) => {
-  const name = pokemon.name;
+  let name = pokemon.name;
+  name = name[0].toUpperCase() + name.substring(1);
   const url = pokemon.url;
   const pokemonIndex = url.split("/")[url.split("/").length - 2];
   const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonIndex}.png`;
 
   return (
     <div className="p-5">
-      <h1 className="text-3xl font-bold">{name}</h1>
-      <Image src={imageUrl} width="200" height="200" className="" />
+      <h1 className="text-center text-3xl font-bold">{name}</h1>
+      <Image src={imageUrl} width="250" height="250" />
     </div>
   );
 };
