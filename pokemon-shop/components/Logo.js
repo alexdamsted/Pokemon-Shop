13ded78ea29e881609df5e36b3d8c01a5e265d2f
alex damsted <alexdamsted@gmail.com>
2021-07-26 17:52:07 +1200

    Change Logo font size

--- pokemon-shop/components/Logo.js
+++ pokemon-shop/components/Logo.js
@@ -1,7 +1,9 @@
 function Logo({ text }) {
   return (
     <div>
-      <h1 className="mt-5 md:m-0 text-5xl font-bold lg:text-6xl">{text}</h1>
+      <h1 className="my-10 text-blue-500 text-5xl font-bold md:m-0 lg:text-5xl">
+        {text}
+      </h1>
     </div>
   );
 }
