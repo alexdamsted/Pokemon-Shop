c08c789683d50eb4a86cb035ba5f51d832ecc712
alex damsted <alexdamsted@gmail.com>
2021-07-27 14:40:13 +1200

    Make the search bar look better!

--- pokemon-shop/components/Search.js
+++ pokemon-shop/components/Search.js
@@ -1,14 +1,13 @@
 function Search({ handleChange, Icon, placeholder }) {
   return (
     <div className="flex items-center w-full">
-      {" "}
       <input
         type="text"
-        className="w-full text-center text-2xl text-gray-500 py-3 rounded z-0 focus:shadow focus:outline-none md:text-2xl md:py-3"
+        className="bg-gray-200 w-full text-center 
+        text-2xl py-3 z-0 focus:border focus:border-black focus:outline-none md:text-lg md:py-3 md: px-10 placeholder-gray-700"
         placeholder={placeholder}
         onChange={handleChange}
       />
-      <Icon className="absolute ml-1 h-6 w-6 text-gray-400 z-20 hover:text-gray-500"></Icon>
     </div>
   );
 }
