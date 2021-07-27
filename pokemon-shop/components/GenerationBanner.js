7b39c4528a58f64774610c6a547efd0bcbee03fa
alex damsted <alexdamsted@gmail.com>
2021-07-27 18:06:40 +1200

    Alt tag added to banner image component

--- pokemon-shop/components/GenerationBanner.js
+++ pokemon-shop/components/GenerationBanner.js
@@ -14,7 +14,7 @@ const GenerationBanner = ({
 
   return (
     <div className="relative mt-32 m-4">
-      <Image src={bannerImage} width="700" height="1000" />
+      <Image src={bannerImage} alt="banner image" width="700" height="1000" />
       <div className="absolute left-0 top-0 right-0 bottom-0 flex flex-col justify-center items-center">
         <Image src={getSpriteUrl(bannerPokemon.url)} width="250" height="250" />
         <h1
