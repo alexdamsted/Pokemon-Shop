bfb394952320a6371027b4bd856b1d3784f260f6
alex damsted <alexdamsted@gmail.com>
2021-07-27 18:10:16 +1200

    Remove Logo component margin on small devices

--- pokemon-shop/components/Logo.js
+++ pokemon-shop/components/Logo.js
@@ -4,7 +4,7 @@ function Logo({ text }) {
   return (
     <div className="flex gap-2">
       <Image src="/../public/pokeball-48.png" width="48" height="48" />
-      <h1 className="my-10 text-black text-2xl font-bold md:m-0 lg:text-2xl">
+      <h1 className="text-black text-2xl font-bold md:m-0 lg:text-2xl">
         {text}
       </h1>
     </div>
