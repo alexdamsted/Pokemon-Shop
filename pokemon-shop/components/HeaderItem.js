d222bbfd842e8b23d9b9f734c421a4f20a109581
alex damsted <alexdamsted@gmail.com>
2021-07-27 15:07:54 +1200

    Bring back icon text to header items

--- pokemon-shop/components/HeaderItem.js
+++ pokemon-shop/components/HeaderItem.js
@@ -1,9 +1,9 @@
 function HeaderItem({ title, Icon }) {
   return (
     <>
-      <div className="text-gray-500 flex items-center my-5 cursor-pointer group hover:text-gray-400 hover:underline">
+      <div className="text-gray-700 flex items-center my-5 cursor-pointer group hover:text-gray-400 hover:underline">
         <Icon className="h-8" />
-        {/* <p className="ml-1 text-xl font-light sm:text-2xl">{title}</p> */}
+        <p className="ml-1 text-sm sm:text-sm">{title}</p>
       </div>
     </>
   );
