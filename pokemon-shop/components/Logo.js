8f4e5760fc19aeaa492781a6685ac32c13b2997d
alex damsted <alexdamsted@gmail.com>
2021-07-29 11:59:43 +1200

    Logo image and text weren't vertically centered

--- pokemon-shop/components/Logo.js
+++ pokemon-shop/components/Logo.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function Logo({ text }) {
   return (
-    <div className="flex gap-2">
+    <div className="flex justify-center items-center gap-2">
       <Image src="/pokeball-48.png" alt="pokeball" width="48" height="48" />
       <h1 className="text-black text-2xl font-bold md:m-0">{text}</h1>
     </div>
