852209100456c39897ed1c8b3403fcc5a5837311
alex damsted <alexdamsted@gmail.com>
2021-07-29 18:02:45 +1200

    Generate html pages for each dynamic route by name

--- pokemon-shop/pages/[id].js
+++ pokemon-shop/pages/[id].js
@@ -1,7 +1,32 @@
-const PokemonDetails = () => {
+export const getStaticPaths = async () => {
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=2000/");
+  const pokemonData = await res.json();
+
+  const paths = pokemonData.results.map((pokemon) => {
+    return { params: { id: pokemon.name.toString() } }; // id is a mandatory object name in getStaticPaths functions
+  });
+
+  return {
+    paths: paths,
+    fallback: false, // Show 404 page when accessing a non-existent route
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const name = context.params.id;
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon/" + name);
+  const pokemonData = await res.json();
+
+  return {
+    props: { pokemon: pokemonData },
+  };
+};
+
+const PokemonDetails = ({ pokemon }) => {
   return (
     <div>
       <p>Pokemon Details Page</p>
+      <p>{pokemon.id}</p>
     </div>
   );
 };
