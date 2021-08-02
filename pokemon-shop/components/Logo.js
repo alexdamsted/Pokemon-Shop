8821dae91203392c6e1ee28d6d9be2b60468351a
alex damsted <alexdamsted@gmail.com>
2021-08-03 10:11:27 +1200

    Functional components aren't allowed as Link child

--- pokemon-shop/components/Logo.js
+++ pokemon-shop/components/Logo.js
@@ -5,8 +5,11 @@ function Logo({ text }) {
   return (
     <div className="flex justify-center items-center gap-2 hover:cursor-pointer">
       <Link href="/">
-        <Image src="/pokeball-48.png" alt="pokeball" width="48" height="48" />
+        <a>
+          <Image src="/pokeball-48.png" alt="pokeball" width="48" height="48" />
+        </a>
       </Link>
+
       <Link href="/">
         <h1 className="text-black text-2xl font-bold md:m-0">{text}</h1>
       </Link>
