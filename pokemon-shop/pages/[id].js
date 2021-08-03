bb3252d18969c4f4040443785558f158a7e7db0d
alex damsted <alexdamsted@gmail.com>
2021-08-03 13:03:17 +1200

    Place Pokemon moves in a <ul>

--- pokemon-shop/pages/[id].js
+++ pokemon-shop/pages/[id].js
@@ -88,7 +88,7 @@ const PokemonDetails = ({ pokemon }) => {
             >
               Moves
               <svg
-                class="fill-current h-4 w-4"
+                className="fill-current h-4 w-4"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
               >
@@ -96,9 +96,11 @@ const PokemonDetails = ({ pokemon }) => {
               </svg>
             </p>
 
-            <p className={isToggleOn ? "font-light" : "font-light hidden "}>
-              {pokemon.moves.map((moves) => moves.move.name + " ")}
-            </p>
+            <ul className={isToggleOn ? "font-light" : "font-light hidden"}>
+              {pokemon.moves.map((moves) => (
+                <li>{moves.move.name}</li>
+              ))}
+            </ul>
             <p className="font-light">
               Stats
               {/* {pokemon.stats.map(
