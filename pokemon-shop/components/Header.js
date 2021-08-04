d055ab75da0f3f8808c36f106cd3283e1d4eb32a
alex damsted <alexdamsted@gmail.com>
2021-08-04 14:29:01 +1200

    Logo positioned at start, header items at end

--- pokemon-shop/components/Header.js
+++ pokemon-shop/components/Header.js
@@ -12,12 +12,12 @@ import Search from "./Search";
 function Header({ handleChange }) {
   return (
     <>
-      <header className="fixed w-full z-50 bg-white py-4 border-b border-gray-300">
-        <div className="2xl:flex 2xl:justify-around">
-          <div className="flex justify-center items-center py-5 2xl:p-0">
-            <Logo text="Pokémon Shop" />
-          </div>
-          <div className="flex justify-center">
+      <header className="py-4 px-8 flex justify-between fixed w-full z-50 bg-white border-b border-gray-300">
+        <div className="flex">
+          <Logo text="Pokémon Shop" />
+        </div>
+        <div className="2xl:flex 2xl:justify-end">
+          <div className="2xl:mr-10">
             <Search
               handleChange={handleChange}
               Icon={SearchIcon}
--- pokemon-shop/components/Logo.js
+++ pokemon-shop/components/Logo.js
@@ -3,15 +3,15 @@ import Link from "next/link";
 
 function Logo({ text }) {
   return (
-    <div className="flex justify-center items-center gap-2 hover:cursor-pointer">
+    <div className="flex gap-1 hover:cursor-pointer">
       <Link href="/" passHref>
-        <a>
+        <a className="flex items-center">
           <Image src="/pokeball-48.png" alt="pokeball" width="48" height="48" />
         </a>
       </Link>
 
       <Link href="/" passHref>
-        <h1 className="text-black text-lg font-montserrat font-semibold md:m-0">
+        <h1 className="flex items-center text-black text-lg font-montserrat font-semibold md:m-0">
           {text}
         </h1>
       </Link>
