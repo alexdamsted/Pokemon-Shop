fea9c8bc0a3d3c1745290a7aac1f92eb35b349ab
alex damsted <alexdamsted@gmail.com>
2021-08-05 11:23:05 +1200

    Search Bar button bg-black + bg-gray-100

--- pokemon-shop/components/Search.js
+++ pokemon-shop/components/Search.js
@@ -1,14 +1,14 @@
 function Search({ handleChange, Icon, placeholder }) {
   return (
-    <div className="bg-gray-200 flex items-center">
+    <div className="bg-gray-100 flex items-center">
       <input
         type="text"
-        className="text-left font-opensans font-normal bg-gray-200 text-sm
+        className="text-left font-opensans font-normal bg-gray-100 text-sm
         py-2 px-4 w-full z-0 focus:outline-none placeholder-gray-700 text-gray-700"
         placeholder={placeholder}
         onChange={handleChange}
       />
-      <button className="bg-gray-500 p-3 flex justify-end items-center text-white transition ease-in-out duration-300 hover:bg-blue-600">
+      <button className="bg-black p-3 flex justify-end items-center text-white transition ease-in-out duration-300 hover:bg-blue-600">
         <Icon className="h-5" />
       </button>
     </div>
