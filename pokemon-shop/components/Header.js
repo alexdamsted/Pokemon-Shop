e6b81fd34390344b685dbd70cca992e8508fe8df
alex damsted <alexdamsted@gmail.com>
2021-08-04 14:41:05 +1200

    Header responsiveness was out of wack after change

--- pokemon-shop/components/Header.js
+++ pokemon-shop/components/Header.js
@@ -12,8 +12,8 @@ import Search from "./Search";
 function Header({ handleChange }) {
   return (
     <>
-      <header className="py-4 px-8 flex justify-between fixed w-full z-50 bg-white border-b border-gray-300">
-        <div className="flex">
+      <header className="py-4 px-8 2xl:flex 2xl:justify-between fixed w-full z-50 bg-white border-b border-gray-300">
+        <div className="flex justify-center mb-4 2xl:m-0">
           <Logo text="Pokémon Shop" />
         </div>
         <div className="2xl:flex 2xl:justify-end">
@@ -24,7 +24,7 @@ function Header({ handleChange }) {
               placeholder="Pokémon Search..."
             />
           </div>
-          <div className="flex justify-center items-center flex-col my-4 sm:flex-row sm:gap-8 sm:m-0">
+          <div className="flex justify-center items-center flex-col mt-4 lg:flex-row lg:gap-8 2xl:m-0">
             <HeaderItem title="MY BAG" Icon={ShoppingBagIcon} />
             <HeaderItem title="SAVED ITEMS" Icon={HeartIcon} />
             <HeaderItem title="SIGN IN / REGISTER" Icon={UserCircleIcon} />
