984df4e48d788156a37cf08800337bfabc7db5ad
alex damsted <alexdamsted@gmail.com>
2021-08-04 12:32:14 +1200

    Change size/color/font family of header items

--- pokemon-shop/components/HeaderItem.js
+++ pokemon-shop/components/HeaderItem.js
@@ -1,9 +1,9 @@
 function HeaderItem({ title, Icon }) {
   return (
     <>
-      <div className="text-gray-700 flex items-center my-2 cursor-pointer group hover:text-gray-400 hover:underline sm:my-5 xl:m-0">
+      <div className="text-gray-500 flex items-center my-2 cursor-pointer group transition ease-in-out duration-300 hover:text-blue-600 sm:my-5 xl:m-0">
         <Icon className="h-8" />
-        <p className="ml-1 text-sm sm:text-sm">{title}</p>
+        <p className="font-montserrat ml-1 font-bold text-xs">{title}</p>
       </div>
     </>
   );
