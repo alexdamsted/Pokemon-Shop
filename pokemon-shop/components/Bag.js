110d70db1ef650144d672268c20baa054bcd0568
alex damsted <alexdamsted@gmail.com>
2021-08-05 15:25:24 +1200

    Slide animation as Bag opens and also as it closes

--- pokemon-shop/components/Bag.js
+++ pokemon-shop/components/Bag.js
@@ -3,8 +3,8 @@ const Bag = ({ isCartClicked }) => {
     <div
       className={
         isCartClicked
-          ? "bg-gray-500 h-full w-1/5 fixed top-0 right-0 z-50 pt-96 overflow-x-hidden transition duration-150"
-          : "hidden"
+          ? "bg-gray-500 h-full w-1/5 fixed top-0 right-0 z-50 pt-96 overflow-x-hidden duration-300"
+          : "bg-gray-500 h-full w-0 fixed top-0 right-0 z-50 pt-96 overflow-x-hidden duration-300"
       }
     ></div>
   );
