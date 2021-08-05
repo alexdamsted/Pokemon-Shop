f34714c7ce8d18549bb4c4ee3bcb934036f858cd
alex damsted <alexdamsted@gmail.com>
2021-08-05 15:36:32 +1200

    bg-white on Bag, no longer gray

--- pokemon-shop/components/Bag/Bag.js
+++ pokemon-shop/components/Bag/Bag.js
@@ -6,8 +6,8 @@ const Bag = ({ isCartClicked, handleCartClick }) => {
     <div
       className={
         isCartClicked
-          ? "bg-gray-500 h-full w-1/5 fixed top-0 right-0 z-50 pt-96 overflow-x-hidden duration-300"
-          : "bg-gray-500 h-full w-0 fixed top-0 right-0 z-50 pt-96 overflow-x-hidden duration-300"
+          ? "bg-white h-full w-1/5 fixed top-0 right-0 z-50 pt-96 overflow-x-hidden duration-300"
+          : "bg-white h-full w-0 fixed top-0 right-0 z-50 pt-96 overflow-x-hidden duration-300"
       }
     >
       <CloseButton Icon={XIcon} handleCartClick={handleCartClick} />
