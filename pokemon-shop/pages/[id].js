fbf3e9da5b06298cf2203e7ce7a036f6ab4dbb30
alex damsted <alexdamsted@gmail.com>
2021-08-05 16:40:16 +1200

    Bag now functions the same on all current pages

--- pokemon-shop/pages/[id].js
+++ pokemon-shop/pages/[id].js
@@ -27,7 +27,12 @@ export const getStaticProps = async (context) => {
   };
 };
 
-const PokemonDetails = ({ pokemon, isCartClicked, handleCartClick }) => {
+const PokemonDetails = ({
+  pokemon,
+  handleChange,
+  isCartClicked,
+  handleCartClick,
+}) => {
   const [isToggleOn, setIsToggleOn] = useState(false);
 
   const transformFirstLetterUppercase = (name) => {
@@ -83,7 +88,7 @@ const PokemonDetails = ({ pokemon, isCartClicked, handleCartClick }) => {
 
   return (
     <>
-      <Header />
+      <Header handleChange={handleChange} handleCartClick={handleCartClick} />
       <Bag isCartClicked={isCartClicked} handleCartClick={handleCartClick} />
       <div className="flex flex-col items-center justify-center pt-72 2xl:pt-32 2xl:w-2/3 2xl:m-auto 2xl:flex 2xl:flex-row 2xl:items-start 2xl:justify-center">
         <div className="2xl:w-1/2 2xl:flex 2xl:items-center 2xl:justify-center">
