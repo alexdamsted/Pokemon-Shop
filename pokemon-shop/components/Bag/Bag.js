ed7ac3ff9a4d47f0e7df5227a7f0eee534a0cefa
alex damsted <alexdamsted@gmail.com>
2021-08-07 17:46:10 +1200

    Border-left 1px added to the Bag

--- pokemon-shop/components/Bag/Bag.js
+++ pokemon-shop/components/Bag/Bag.js
@@ -16,7 +16,7 @@ const Bag = ({
     <div
       className={
         isCartClicked
-          ? "border bg-white h-full w-1/5 opacity-100 fixed top-0 p-3 right-0 z-50 overflow-x-hidden duration-300"
+          ? "border-l bg-white h-full w-1/5 opacity-100 fixed top-0 p-3 right-0 z-50 overflow-x-hidden duration-300"
           : "bg-white h-full w-0 fixed top-0 right-0 z-50 overflow-x-hidden duration-300"
       }
     >
