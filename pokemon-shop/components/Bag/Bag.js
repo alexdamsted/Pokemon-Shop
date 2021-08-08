2c22bc774a2db88a909e85aff2ee6abd63a2eb8a
alex damsted <alexdamsted@gmail.com>
2021-08-08 13:33:28 +1200

    Bag was not responsive, it is now

--- pokemon-shop/components/Bag/Bag.js
+++ pokemon-shop/components/Bag/Bag.js
@@ -18,7 +18,7 @@ const Bag = ({
     <div
       className={
         isCartClicked
-          ? "border-l bg-white h-full w-1/5 opacity-100 fixed top-0 p-3 right-0 z-50 overflow-x-hidden duration-300"
+          ? "border-l bg-white h-full w-full opacity-100 fixed top-0 p-3 right-0 z-50 overflow-x-hidden duration-300 xl:w-2/6"
           : "bg-white h-full w-0 fixed top-0 right-0 z-50 overflow-x-hidden duration-300"
       }
     >
