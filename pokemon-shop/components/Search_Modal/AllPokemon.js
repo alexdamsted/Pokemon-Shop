ad8666393fe65b26bffee0b5d03f460854a1c3b3
alex damsted <alexdamsted@gmail.com>
2021-08-12 13:42:02 +1200

    Display Pokemon sprites in Search Model

--- pokemon-shop/components/Search_Modal/AllPokemon.js
+++ pokemon-shop/components/Search_Modal/AllPokemon.js
@@ -1,13 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
 const AllPokemon = ({ allPokemon, inputBox }) => {
-  console.log("yo");
+  const getSpriteUrl = (url) => {
+    const pokemonIndex = url.split("/")[url.split("/").length - 2];
+    const pokemonSpriteUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonIndex}.png`;
+    return pokemonSpriteUrl;
+  };
   return (
     <>
-      <div className="text-xl text-black">
-        <div>
-          <p>Product Suggestions for: {inputBox.inputBox}</p>
-        </div>
+      <div className="mb-5 text-xl text-black">
+        <p>Product Suggestions for: {inputBox.inputBox}</p>
+      </div>
+      <div className="flex flex-wrap">
         {allPokemon.map((allPokemon) => (
-          <p>{allPokemon.name}</p>
+          <div>
+            <p className="text-xl text-black">{allPokemon.name}</p>
+
+            <Image
+              src={getSpriteUrl(allPokemon.url)}
+              alt="pokemon"
+              width="150"
+              height="150"
+              className="transition duration-200 transform hover:scale-125 hover:cursor-pointer"
+            />
+          </div>
         ))}
       </div>
     </>
