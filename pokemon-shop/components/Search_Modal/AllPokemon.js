9798c7df30d8bb9f1037ff5790c05fbeefae94ed
alex damsted <alexdamsted@gmail.com>
2021-08-12 13:26:47 +1200

    Use input box to display what user is looking for

--- pokemon-shop/components/Search_Modal/AllPokemon.js
+++ pokemon-shop/components/Search_Modal/AllPokemon.js
@@ -1,8 +1,11 @@
-const AllPokemon = ({ allPokemon }) => {
+const AllPokemon = ({ allPokemon, inputBox }) => {
   console.log("yo");
   return (
     <>
       <div className="text-xl text-black">
+        <div>
+          <p>Product Suggestions for: {inputBox.inputBox}</p>
+        </div>
         {allPokemon.map((allPokemon) => (
           <p>{allPokemon.name}</p>
         ))}
